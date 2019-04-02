4e5498c408e4687eacdac6ee6e3b6425fd9fbf37
Cassiano Sampaio <cassianobsampaio@gmail.com>
2019-04-02 13:59:41 -0300

    error for review

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,10 @@
 class ArticlesController < ApplicationController
 
   def index
-      articles = Article.recent.page(current_page).per(per_page)
-      render json: serializer.new(articles)
-	end
-
-  # def index
-  #   paginated = Article.recent.page(current_page).per(per_page)
-  #   options = PaginationMetaGenerator.new(request: request, total_pages: paginated.total_pages).call()
-  #   render json: serializer.new(paginated, options)
-  # end
+    paginated = Article.recent.page(current_page).per(per_page)
+    options = PaginationMetaGenerator.new(request: request, total_pages: paginated.total_pages).call()
+    render json: serializer.new(paginated, options)
+  end
 
 	def show
     render json: serializer.new(Article.find(params[:id]))
